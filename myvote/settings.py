f311ef570143b97aa8e7f12f3d36203229b534dd
elliot_alderson <elliot_alderson@MR.robot>
2025-11-02 19:19:06 +0330

    fix: static files path for Render

--- myvote/settings.py
+++ myvote/settings.py
@@ -128,7 +128,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates', 'vote', 'static'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 
